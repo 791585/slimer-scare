54d3e3d3e50f87df47a5388da5d44ac23f3e5b71
paige <127938204+791585@users.noreply.github.com>
2023-04-27 14:18:59 +0100

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 let mySprite: Sprite = null
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite = sprites.create(assets.image`slimer`, SpriteKind.Player)
+    scene.setBackgroundImage(assets.image`apartment`)
 })
