7bce231964a00ccc07c9fadc7072501e39188a38
paige <127938204+791585@users.noreply.github.com>
2023-05-17 13:42:56 +0100

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
-controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
-        mySprite.vy = -200
-    }
-})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile6`, function (sprite, location) {
     scene.setBackgroundImage(assets.image`lobby`)
     tiles.setCurrentTilemap(tilemap`level`)
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    let if_right = false
+    if (if_right == false) {
+        projectile = sprites.createProjectileFromSprite(assets.image`myImage`, mySprite, -50, 0)
+    } else if (if_right == true) {
+        projectile = sprites.createProjectileFromSprite(assets.image`myImage`, mySprite, 50, 0)
+    }
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile15`, function (sprite, location) {
     info.changeLifeBy(-1)
@@ -28,6 +31,9 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile12`, function (sprite, 
     tiles.setTileAt(location, assets.tile`transparency16`)
     tiles.setTileAt(location, assets.tile`transparency16`)
 })
+scene.onOverlapTile(SpriteKind.Enemy, assets.tile`myTile19`, function (sprite, location) {
+    statusbar.value += 0
+})
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.setImage(assets.image`slimer`)
 })
@@ -35,11 +41,24 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile9`, function (sprite, l
     tiles.setTileAt(location, assets.tile`transparency16`)
     info.changeScoreBy(1)
 })
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile18`, function (sprite, location) {
+    myEnemy = sprites.create(assets.image`myImage1`, SpriteKind.Player)
+    info.changeLifeBy(-1)
+    if (!(myEnemy)) {
+        statusbar.setColor(10, 5)
+        statusbar.value = 100
+        statusbar.value += -1
+    }
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile16`, function (sprite, location) {
     info.changeLifeBy(-1)
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.setImage(assets.image`slimer1`)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    statusbar.value += -5
+    sprites.destroy(projectile)
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile17`, function (sprite, location) {
     scene.setBackgroundImage(assets.image`apartment`)
@@ -57,8 +76,123 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile10`, function (sprite, 
     scene.setBackgroundImage(assets.image`roof`)
     tiles.setCurrentTilemap(tilemap`level5`)
     mySprite.setVelocity(0, 50)
-    mySprite2 = sprites.create(assets.image`myImage0`, SpriteKind.Player)
     tiles.setTileAt(tiles.getTileLocation(14, 2), assets.tile`myTile18`)
+    statusbar = statusbars.create(20, 4, StatusBarKind.Health)
+    myEnemy.follow(mySprite, 50)
+    statusbar.attachToSprite(myEnemy)
+    statusbar.setLabel("HP")
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile23`, function (sprite, location) {
+    tiles.setTileAt(location, assets.tile`transparency16`)
+    info.changeLifeBy(1)
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . 5 5 5 5 5 . . . . . . . 
+        . . . 4 4 7 4 4 6 5 . . . . . . 
+        . . 5 4 5 6 5 4 7 6 5 . . . . . 
+        . 5 7 7 7 6 7 7 7 7 7 5 . . . . 
+        . 5 6 7 7 7 7 7 7 7 7 7 . . . . 
+        8 8 5 7 1 2 2 2 2 2 7 7 7 5 . . 
+        4 a 8 2 3 3 2 3 3 2 5 7 5 7 5 . 
+        a 4 a 8 3 3 3 2 2 7 5 7 7 6 5 . 
+        8 4 4 a 8 5 7 7 7 5 7 7 5 6 6 . 
+        . 8 a 4 8 7 7 7 7 6 6 6 6 6 6 5 
+        . . 8 8 5 5 5 5 5 5 6 6 6 6 6 . 
+        . . . . . . 6 6 6 6 6 6 6 6 5 . 
+        . . . . . . . 5 5 5 6 6 5 5 . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . d d d d d d d d d d d 
+        . . . . . . . d d d d d d d . . 
+        `,img`
+        . . . . 5 5 5 5 5 . . . . . . . 
+        . . . 4 4 7 4 4 6 5 . . . . . . 
+        . . 5 4 5 6 5 4 7 6 5 . . . . . 
+        . 5 7 7 7 6 7 7 7 7 7 5 . . . . 
+        . 5 6 7 7 7 7 7 7 7 7 7 . . . . 
+        . 8 8 7 1 2 2 2 2 2 7 7 7 5 . . 
+        8 4 a 8 3 3 2 3 3 2 5 7 5 7 5 . 
+        8 a 4 a 8 3 3 2 2 7 5 7 7 6 5 . 
+        . 8 4 4 a 8 7 7 7 5 7 7 5 6 6 . 
+        . . 8 a 4 8 7 7 7 6 6 6 6 6 6 5 
+        . . . 8 8 5 5 5 5 5 6 6 6 6 6 . 
+        . . . . . . 6 6 6 6 6 6 6 6 5 . 
+        . . . . . . . 5 5 5 6 6 5 5 . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . d d d d d d d d d d d 
+        . . . . . . . d d d d d d d . . 
+        `,img`
+        . . . . 5 5 5 5 5 . . . . . . . 
+        . . . 4 4 7 4 4 6 5 . . . . . . 
+        . . 5 4 5 6 5 4 7 6 5 . . . . . 
+        . 5 7 7 7 6 7 7 7 7 7 5 . . . . 
+        . 5 6 7 7 7 7 7 7 7 7 7 . . . . 
+        . 5 8 8 1 2 2 2 2 2 7 7 7 5 . . 
+        . 8 4 a 8 3 2 3 3 2 5 7 5 7 5 . 
+        . 8 a 4 a 8 3 2 2 7 5 7 7 6 5 . 
+        . . 8 4 4 a 8 7 7 5 7 7 5 6 6 . 
+        . . . 8 a 4 8 7 7 6 6 6 6 6 6 5 
+        . . . . 8 5 5 5 5 5 6 6 6 6 6 . 
+        . . . . . . 6 6 6 6 6 6 6 6 5 . 
+        . . . . . . . 5 5 5 6 6 5 5 . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . d d d d d d d d d d d 
+        . . . . . . . d d d d d d d . . 
+        `,img`
+        . . . . 5 5 5 5 5 . . . . . . . 
+        . . . 4 4 7 4 4 6 5 . . . . . . 
+        . . 5 4 5 6 5 4 7 6 5 . . . . . 
+        . 5 7 7 7 6 7 7 7 7 7 5 . . . . 
+        . 5 6 7 7 7 7 7 7 7 7 7 . . . . 
+        . 5 7 8 8 8 8 2 2 2 7 7 7 5 . . 
+        . 5 7 4 a a a a 3 2 5 7 5 7 5 . 
+        . 5 7 a 4 a 4 a a 7 5 7 7 6 5 . 
+        . . 5 7 4 4 a 8 7 5 7 7 5 6 6 . 
+        . . . 5 8 a 4 8 7 6 6 6 6 6 6 5 
+        . . . . 5 7 5 5 5 5 6 6 6 6 6 . 
+        . . 8 . . 5 6 6 6 6 6 6 6 6 5 . 
+        . . . 8 . . 5 5 5 5 6 6 5 5 . . 
+        . . . . . . . . . . . . . . . . 
+        . 8 . . . d d d d d d d d d d d 
+        . . . . . . . d d d d d d d . . 
+        `,img`
+        . . . . 5 5 5 5 5 . . . . . . . 
+        . . . 4 4 7 4 4 6 5 . . . . . . 
+        . . 5 4 5 6 5 4 7 6 5 . . . . . 
+        . 5 7 7 7 6 7 7 7 7 7 5 . . . . 
+        . 5 6 7 7 7 7 7 7 7 7 7 . . . . 
+        . 5 7 7 8 8 8 8 8 8 7 7 7 5 . . 
+        . 5 7 7 a a a a 3 8 5 7 5 7 5 . 
+        . 5 7 8 4 a 4 a a 7 5 7 7 6 5 . 
+        . . 5 7 8 4 a 8 7 5 7 7 5 6 6 . 
+        . . . 5 7 8 4 8 7 6 6 6 6 6 6 5 
+        . . . . 5 7 5 5 5 5 6 6 6 6 6 . 
+        . . 8 . . 5 6 6 6 6 6 6 6 6 5 . 
+        . . . 8 . . 5 5 5 5 6 6 5 5 . . 
+        . . . . . . . . . . . . . . . . 
+        . 8 . . . d d d d d d d d d d d 
+        . . . . . . . d d d d d d d . . 
+        `,img`
+        . . . . 5 5 5 5 5 . . . . . . . 
+        . . . 4 4 7 4 4 6 5 . . . . . . 
+        . . 5 4 5 6 5 4 7 6 5 . . . . . 
+        . 5 7 7 7 6 7 7 7 7 7 5 . . . . 
+        . 5 6 7 7 7 7 7 7 7 7 7 . . . . 
+        . 5 7 7 7 7 7 7 2 7 7 7 7 5 . . 
+        . 5 7 2 2 1 2 2 2 7 5 7 5 7 5 . 
+        . 5 7 2 2 2 2 2 7 7 5 7 7 6 5 . 
+        . . 5 7 7 7 7 7 7 5 7 7 5 6 6 . 
+        . . . 5 7 7 7 7 7 6 6 6 6 6 6 5 
+        . . . . 5 7 5 5 5 5 6 6 6 6 6 . 
+        . . 8 . . 5 6 6 6 6 6 6 6 6 5 . 
+        . . . 8 . . 5 5 5 5 6 6 5 5 . . 
+        . . . . . . . . . . . . . . . . 
+        . 8 . . . d d d d d d d d d d d 
+        . . . . . . . d d d d d d d . . 
+        `],
+    500,
+    false
+    )
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile14`, function (sprite, location) {
     info.changeLifeBy(-1)
@@ -67,7 +201,9 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile14`, function (sprite, 
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile11`, function (sprite, location) {
     info.changeLifeBy(-1)
 })
-let mySprite2: Sprite = null
+let myEnemy: Sprite = null
+let statusbar: StatusBarSprite = null
+let projectile: Sprite = null
 let mySprite: Sprite = null
 mySprite = sprites.create(assets.image`slimer`, SpriteKind.Player)
 scene.cameraFollowSprite(mySprite)
